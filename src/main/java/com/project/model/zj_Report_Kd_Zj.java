ab8c33e4deaf1368a355eacdf734641c60e78eda
xfy654305690 <654305690@qq.com>
2022-02-17 00:07:05 +0800

    变更

---
+++ src/main/java/com/project/model/zj_Report_Kd_Zj.java
@@ -0,0 +1,176 @@
+package com.project.model;
+
+public class zj_Report_Kd_Zj {
+
+    //支局全程 Zj_Abbr_Name
+    public  String Zj_Name;
+    //宽带新增数量
+    public  Integer bb_Amt;
+    //宽带新增日均数量
+    public  Double  bb_Amt_Avg;
+    //宽带新增指标
+    public  Double  bb_Amt_Tar;
+    //宽宽带新增日均指标
+    public  Double  bb_Amt_Avg_Tar;
+    //宽带新增缺口
+    public  Integer  bb_Amt_Gap;
+    //宽带新增日均缺口
+    public  Double  bb_Amt_Avg_Gap;
+    //宽带新增日均完成率
+    public  Double  bb_Amt_Rate;
+    //宽带新增数量
+    public  Integer bb_Amt_Jz;
+    //宽带新增日均数量
+    public  Double  bb_Amt_Avg_Jz;
+    //宽带新增指标
+    public  Double  bb_Amt_Tar_Jz;
+    //宽宽带新增日均指标
+    public  Double  bb_Amt_Avg_Tar_Jz;
+    //宽带新增缺口
+    public  Integer  bb_Amt_Gap_Jz;
+    //宽带新增日均缺口
+    public  Double  bb_Amt_Avg_Gap_Jz;
+    //宽带新增日均完成率
+    public  Double  bb_Amt_Rate_Jz;
+
+    public String getZj_Name() {
+        return Zj_Name;
+    }
+
+    public void setZj_Name(String zj_Name) {
+        Zj_Name = zj_Name;
+    }
+
+    public Integer getBb_Amt() {
+        return bb_Amt;
+    }
+
+    public void setBb_Amt(Integer bb_Amt) {
+        this.bb_Amt = bb_Amt;
+    }
+
+    public Double getBb_Amt_Avg() {
+        return bb_Amt_Avg;
+    }
+
+    public void setBb_Amt_Avg(Double bb_Amt_Avg) {
+        this.bb_Amt_Avg = bb_Amt_Avg;
+    }
+
+    public Double getBb_Amt_Tar() {
+        return bb_Amt_Tar;
+    }
+
+    public void setBb_Amt_Tar(Double bb_Amt_Tar) {
+        this.bb_Amt_Tar = bb_Amt_Tar;
+    }
+
+    public Double getBb_Amt_Avg_Tar() {
+        return bb_Amt_Avg_Tar;
+    }
+
+    public void setBb_Amt_Avg_Tar(Double bb_Amt_Avg_Tar) {
+        this.bb_Amt_Avg_Tar = bb_Amt_Avg_Tar;
+    }
+
+    public Integer getBb_Amt_Gap() {
+        return bb_Amt_Gap;
+    }
+
+    public void setBb_Amt_Gap(Integer bb_Amt_Gap) {
+        this.bb_Amt_Gap = bb_Amt_Gap;
+    }
+
+    public Double getBb_Amt_Avg_Gap() {
+        return bb_Amt_Avg_Gap;
+    }
+
+    public void setBb_Amt_Avg_Gap(Double bb_Amt_Avg_Gap) {
+        this.bb_Amt_Avg_Gap = bb_Amt_Avg_Gap;
+    }
+
+    public Double getBb_Amt_Rate() {
+        return bb_Amt_Rate;
+    }
+
+    public void setBb_Amt_Rate(Double bb_Amt_Rate) {
+        this.bb_Amt_Rate = bb_Amt_Rate;
+    }
+
+    public Integer getBb_Amt_Jz() {
+        return bb_Amt_Jz;
+    }
+
+    public void setBb_Amt_Jz(Integer bb_Amt_Jz) {
+        this.bb_Amt_Jz = bb_Amt_Jz;
+    }
+
+    public Double getBb_Amt_Avg_Jz() {
+        return bb_Amt_Avg_Jz;
+    }
+
+    public void setBb_Amt_Avg_Jz(Double bb_Amt_Avg_Jz) {
+        this.bb_Amt_Avg_Jz = bb_Amt_Avg_Jz;
+    }
+
+    public Double getBb_Amt_Tar_Jz() {
+        return bb_Amt_Tar_Jz;
+    }
+
+    public void setBb_Amt_Tar_Jz(Double bb_Amt_Tar_Jz) {
+        this.bb_Amt_Tar_Jz = bb_Amt_Tar_Jz;
+    }
+
+    public Double getBb_Amt_Avg_Tar_Jz() {
+        return bb_Amt_Avg_Tar_Jz;
+    }
+
+    public void setBb_Amt_Avg_Tar_Jz(Double bb_Amt_Avg_Tar_Jz) {
+        this.bb_Amt_Avg_Tar_Jz = bb_Amt_Avg_Tar_Jz;
+    }
+
+    public Integer getBb_Amt_Gap_Jz() {
+        return bb_Amt_Gap_Jz;
+    }
+
+    public void setBb_Amt_Gap_Jz(Integer bb_Amt_Gap_Jz) {
+        this.bb_Amt_Gap_Jz = bb_Amt_Gap_Jz;
+    }
+
+    public Double getBb_Amt_Avg_Gap_Jz() {
+        return bb_Amt_Avg_Gap_Jz;
+    }
+
+    public void setBb_Amt_Avg_Gap_Jz(Double bb_Amt_Avg_Gap_Jz) {
+        this.bb_Amt_Avg_Gap_Jz = bb_Amt_Avg_Gap_Jz;
+    }
+
+    public Double getBb_Amt_Rate_Jz() {
+        return bb_Amt_Rate_Jz;
+    }
+
+    public void setBb_Amt_Rate_Jz(Double bb_Amt_Rate_Jz) {
+        this.bb_Amt_Rate_Jz = bb_Amt_Rate_Jz;
+    }
+
+    @Override
+    public String toString() {
+        return "zj_Report_Kd_Zj{" +
+                "Zj_Name='" + Zj_Name + '\'' +
+                ", bb_Amt=" + bb_Amt +
+                ", bb_Amt_Avg=" + bb_Amt_Avg +
+                ", bb_Amt_Tar=" + bb_Amt_Tar +
+                ", bb_Amt_Avg_Tar=" + bb_Amt_Avg_Tar +
+                ", bb_Amt_Gap=" + bb_Amt_Gap +
+                ", bb_Amt_Avg_Gap=" + bb_Amt_Avg_Gap +
+                ", bb_Amt_Rate=" + bb_Amt_Rate +
+                ", bb_Amt_Jz=" + bb_Amt_Jz +
+                ", bb_Amt_Avg_Jz=" + bb_Amt_Avg_Jz +
+                ", bb_Amt_Tar_Jz=" + bb_Amt_Tar_Jz +
+                ", bb_Amt_Avg_Tar_Jz=" + bb_Amt_Avg_Tar_Jz +
+                ", bb_Amt_Gap_Jz=" + bb_Amt_Gap_Jz +
+                ", bb_Amt_Avg_Gap_Jz=" + bb_Amt_Avg_Gap_Jz +
+                ", bb_Amt_Rate_Jz=" + bb_Amt_Rate_Jz +
+                '}';
+    }
+}
