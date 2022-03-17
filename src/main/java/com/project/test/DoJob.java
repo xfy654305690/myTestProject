c51050345b2ec6701074986be0199458ff07a9d1
xfy654305690 <654305690@qq.com>
2022-03-17 14:46:04 +0800

    修改

--- src/main/java/com/project/test/DoJob.java
+++ src/main/java/com/project/test/DoJob.java
@@ -14,10 +14,10 @@ public class DoJob  implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         doDetail doDetail=new  doDetail();
         try {
-            //doDetail.doDetailTest();
-            Date date = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            System.out.println("JOB1陆小凤"+simpleDateFormat.format(date));
+            doDetail.doDetailTest();
+            //Date date = new Date();
+            //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            //System.out.println("JOB1陆小凤"+simpleDateFormat.format(date));
         } catch (Exception e) {
             System.out.println("报错了");
             e.printStackTrace();
--- src/main/java/com/project/test/doDetail.java
+++ src/main/java/com/project/test/doDetail.java
@@ -25,7 +25,6 @@ public class doDetail {
             String nowDay= dealTime.get_date_By_String_DD();
             System.out.printf("nowDay");
             if (Integer.valueOf(nowDay)>=5){
-                System.out.printf("IN");
                 zj_Report_Xubao_Controller.report_Xubao_Zj();
                 System.out.printf("XB_COMPLETE");
                 zj_Report_Tcf_Controller.report_Tcf_Zj();
@@ -39,7 +38,7 @@ public class doDetail {
                 zj_Report_Kd_Controller.report_Kd_Zj();
                 System.out.printf("KD_COMPLETE");
                 //zj_Report_OtherDone_Controller.report_OtherDone_Zj();
-                System.out.printf("OtherDone_COMPLETE");
+                //System.out.printf("OtherDone_COMPLETE");
             }
 //            if (Integer.valueOf(nowDay)==6){
 //                zj_Report_Xubao_Controller.report_Xubao_Zj_Js();
@@ -56,31 +55,5 @@ public class doDetail {
 
 
         }
-
-    public static void main(String[] args) throws Exception {
-
-
-        zj_Report_Kd_Controller zj_Report_Kd_Controller=new zj_Report_Kd_Controller();
-        zj_Report_RhItv_Controller zj_Report_RhItv_Controller=new zj_Report_RhItv_Controller();
-        zj_Report_Tcf_Controller zj_Report_Tcf_Controller=new zj_Report_Tcf_Controller();
-        zj_Report_Wyj_Controller zj_Report_Wyj_Controller=new zj_Report_Wyj_Controller();
-        zj_Report_Xubao_Controller zj_Report_Xubao_Controller=new zj_Report_Xubao_Controller();
-        zj_Report_Zt_Controller zj_Report_Zt_Controller=new zj_Report_Zt_Controller();
-        zj_Report_OtherDone_Controller zj_Report_OtherDone_Controller=new zj_Report_OtherDone_Controller();
-
-        //获取当前日期DD格式
-        String nowDay= dealTime.get_date_By_String_DD();
-        if (Integer.valueOf(nowDay)>=5){
-            zj_Report_Xubao_Controller.report_Xubao_Zj();
-            zj_Report_Tcf_Controller.report_Tcf_Zj();
-            zj_Report_Zt_Controller.report_Zt_Zj();
-            zj_Report_Wyj_Controller.report_Wyj_Zj();
-            zj_Report_RhItv_Controller.report_RhItv_Zj();
-            zj_Report_Kd_Controller.report_Kd_Zj();
-            zj_Report_OtherDone_Controller.report_OtherDone_Zj();
-        }
-
-
-    }
 
 }
