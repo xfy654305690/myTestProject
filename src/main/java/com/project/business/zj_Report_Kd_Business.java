a90878ec3d56f408ef35b22c621e51fe2ef6cf47
xfy654305690 <654305690@qq.com>
2022-03-18 10:50:18 +0800

    修复全程难看问题

--- src/main/java/com/project/business/zj_Report_Kd_Business.java
+++ src/main/java/com/project/business/zj_Report_Kd_Business.java
@@ -34,7 +34,7 @@ public class zj_Report_Kd_Business {
     //图片地址跟目录
     public static  final  String OutPictureFile="C:\\test\\KD\\PICTURE\\";
     //微信群名称
-    public static  final  String wechartSendName="aiaiai";
+    public static  final  String wechartSendName="春季营销冲锋行动（鄞战2022）";
     public static  final  String wechartPictureAdress="C:\\test\\KD\\";
     //微信群名称
     public static  final  String inExcleDataFile="C:\\Test\\KD\\test.xlsx";
