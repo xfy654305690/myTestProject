d761adfc2a7d355b2d7d26a6781f95bf71c95833
xfy654305690 <654305690@qq.com>
2022-03-29 17:36:04 +0800

    1.增加准实时

--- src/main/java/com/project/test/DoJob1.java
+++ src/main/java/com/project/test/DoJob1.java
@@ -14,10 +14,10 @@ public class DoJob1 implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         doDetail doDetail=new  doDetail();
         try {
-            //doDetail.doDetailTest();
-            Date date = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            System.out.println("JOB风清扬"+simpleDateFormat.format(date));
+            doDetail.doDetailTest();
+            //Date date = new Date();
+            //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            //System.out.println("JOB风清扬"+simpleDateFormat.format(date));
         } catch (Exception e) {
             e.printStackTrace();
         }
