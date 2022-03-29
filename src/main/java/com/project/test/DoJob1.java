08969d513d7bdc8edd7d67bfcd2194b5613ec2e5
xfy654305690 <654305690@qq.com>
2022-03-29 18:29:23 +0800

    增加定时

--- src/main/java/com/project/test/DoJob1.java
+++ src/main/java/com/project/test/DoJob1.java
@@ -15,9 +15,9 @@ public class DoJob1 implements Job {
         doDetail doDetail=new  doDetail();
         try {
             doDetail.doDetailTest();
-            //Date date = new Date();
-            //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            //System.out.println("JOB风清扬"+simpleDateFormat.format(date));
+//            Date date = new Date();
+//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            System.out.println("JOB风清扬"+simpleDateFormat.format(date));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/project/test/Myschedule.java
+++ src/main/java/com/project/test/Myschedule.java
@@ -15,6 +15,8 @@ public class Myschedule {
         // 2、创建JobDetail实例，并与PrintWordsJob类绑定(Job执行内容)
         JobDetail jobDetail = JobBuilder.newJob(DoJob.class)
                 .withIdentity("job1", "group1").build();
+        JobDetail jobDetailZss1 = JobBuilder.newJob(DoJob1.class)
+                .withIdentity("jobZss1", "groupZss1").build();
         // 3、构建Trigger实例,每隔1s执行一次
         Date startDate = new Date();
         startDate.setTime(startDate.getTime() + 5000);
@@ -22,11 +24,20 @@ public class Myschedule {
                 .usingJobData("trigger1", "这是jobDetail1的trigger")
                 .startNow()//立即生效
                 .startAt(startDate)
-                //.withSchedule(CronScheduleBuilder.cronSchedule("/30 * * * * ?"))
+                //.withSchedule(CronScheduleBuilder.cronSchedule("/3 * * * * ?"))
                 .withSchedule(CronScheduleBuilder.cronSchedule("0 30 23 * * ?"))
                 .build();
+        CronTrigger cronTriggerZss1 = TriggerBuilder.newTrigger().withIdentity("triggerZss1", "triggerGroupZss1")
+                .usingJobData("triggerZss1", "这是jobDetail1的triggerZss1")
+                .startNow()//立即生效
+                .startAt(startDate)
+                //.withSchedule(CronScheduleBuilder.cronSchedule("/1 * * * * ?"))
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 15 13,15,17,20 * * ? *"))
+                .build();
+
         //4、执行
         scheduler.scheduleJob(jobDetail, cronTrigger);
+        scheduler.scheduleJob(jobDetailZss1, cronTriggerZss1);
         System.out.println("--------scheduler start ! ------------");
         scheduler.start();
 
