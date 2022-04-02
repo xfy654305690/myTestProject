be7280421147199fe785690cec7720dd5758e83c
xfy654305690 <654305690@qq.com>
2022-04-02 10:35:18 +0800

    准实时修复

--- src/main/java/com/project/test/DoJob1.java
+++ src/main/java/com/project/test/DoJob1.java
@@ -1,5 +1,6 @@
 package com.project.test;
 
+import com.project.controller.zj_Report_Zss_Controller;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -12,12 +13,12 @@ public class DoJob1 implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        doDetail doDetail=new  doDetail();
+        zj_Report_Zss_Controller zj_Report_Zss_Controller=new zj_Report_Zss_Controller();
         try {
-            doDetail.doDetailTest();
-            //Date date = new Date();
-            //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            //System.out.println("JOB风清扬"+simpleDateFormat.format(date));
+            zj_Report_Zss_Controller.report_Zss_Zj();
+//            Date date = new Date();
+//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            System.out.println("JOB风清扬"+simpleDateFormat.format(date));
         } catch (Exception e) {
             e.printStackTrace();
         }
