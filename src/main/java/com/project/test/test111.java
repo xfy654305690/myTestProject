9d7c4dd8c0947efe2b3b5dd8c413272d5de4ba7d
xfy654305690 <654305690@qq.com>
2022-04-26 10:40:38 +0800

    111111

--- src/main/java/com/project/test/test111.java
+++ src/main/java/com/project/test/test111.java
@@ -21,6 +21,13 @@ public class test111 {
         zj_Report_Zt_Controller zj_Report_Zt_Controller=new zj_Report_Zt_Controller();
         zj_Report_Kd_Wg_Controller zj_Report_Kd_Wg_Controller=new zj_Report_Kd_Wg_Controller();
 
+        zj_Report_Xubao_Controller.report_Xubao_Zj();
+        zj_Report_Tcf_Controller.report_Tcf_Zj();
+        zj_Report_Zt_Controller.report_Zt_Zj();
+        zj_Report_Wyj_Controller.report_Wyj_Zj();
+        zj_Report_RhItv_Controller.report_RhItv_Zj();
+        zj_Report_Kd_Controller.report_Kd_Zj();
+        zj_Report_Kd_Wg_Controller.report_Kd_Zj_Gis();
 
 
     }
