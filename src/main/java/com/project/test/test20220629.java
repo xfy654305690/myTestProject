735942ecf61513d6b17707befbc6acf9e12d2209
xfy654305690 <654305690@qq.com>
2022-06-30 16:42:01 +0800

    1.修复准实时语句

--- src/main/java/com/project/test/test20220629.java
+++ src/main/java/com/project/test/test20220629.java
@@ -6,12 +6,12 @@ import javax.mail.MessagingException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
+import java.util.Scanner;
 
 public class test20220629 {
     public static void main(String[] args) throws MessagingException, IOException, ParseException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-
-        zj_Report_Kd_Qz_Controller zj_Report_Kd_Qz_Controller=new zj_Report_Kd_Qz_Controller();
-        zj_Report_Kd_Qz_Controller.report_Kd_Qz_Zj_630();
-
     }
+
+
+
 }
